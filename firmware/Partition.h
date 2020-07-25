29cb087f99120b4224f62d3b3548152e6608dbdf
PlasticHub <baumgart.guenter@gmail.com>
2020-07-25 13:19:21 +0200

    firmware begin

---
+++ firmware/Partition.h
@@ -0,0 +1,19 @@
+#ifndef PARTITION_H
+#define PARTITION_H
+
+#include <max6675.h>
+#include "./config.h"
+#include "./macros.h"
+#include "./time.h"
+
+/***
+ * 
+ * 
+ * */
+class Partition
+{
+    public:
+        
+};
+
+#endif
